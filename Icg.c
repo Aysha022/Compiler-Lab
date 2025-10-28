a66bea8795efc213afe1b62c12199d9b25c027ef
Aysha Naurin <149690596+Aysha022@users.noreply.github.com>
2025-10-29 00:26:07 +0530

    Update Icg.c

--- Icg.c
+++ Icg.c
@@ -1,59 +1,64 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
-#define VARLEN 10
-#define MAXSIZE 50
-char opndstack[MAXSIZE][VARLEN]={'\0'}; //Stores operands
-char opstack[MAXSIZE]={'\0'}; // Stores operators for postfix conversion
-int opstktop = -1, opndstktop=-1;
-char expn[MAXSIZE*5]={'\0'};
 
-int priority(char c) //returns the priority of operators, zero by default
-{
-    return (c=='+'|c=='-')?1:(c=='*'|c=='/')?2:c=='='?-1:0; //equals given lowest precedence
+#define MAX 50
+
+char operands[MAX][10];
+char operators[MAX];
+int topOpnd = -1, topOp = -1;
+
+int precedence(char c) {
+        if(c == '+' || c == '-')
+                return 1;
+        if(c == '*' || c == '/')
+                return 2;
+        if(c == '=')
+                return -1;
+        return 0;
 }
-void icg_from_infix()
-{
-    int i = 0,temp_count=1;
-    char tempopnd[MAXSIZE]={'\0'};
-    while (expn[i]!='\0')
-    {
-        int j=0;
-        for(;isalnum(expn[i]);)
-            tempopnd[j++]=expn[i++];
-        if(j!=0)//Only if there is an operand 
-            strcpy(opndstack[++opndstktop],tempopnd);
-        if(expn[i]==' ') //Ignore any whitespace
-        {
-            i++;
-            continue;
-        } //pop current operator if the incoming opertator has equal or lower
-        if((opndstktop>=0&&priority(opstack[opstktop])<priority(expn[i]))||opstktop<0)
-            opstack[++opstktop] = expn[i++];
-        else if(opstktop>=0) //pop remaining operators
-        {
-            char op[2]={'\0'};
-            op[0] = opstack[opstktop--];
-            printf("t%d = %s %s %s\n",temp_count,opndstack[opndstktop-1],op,opndstack[opndstktop]);
-            sprintf(opndstack[--opndstktop],"t%d",temp_count++);
+
+void generateICG(char exp[]) {
+        int i = 0, t = 1;
+        char temp[10];
+
+        while(exp[i] != '\0') {
+                if(isalnum(exp[i])) {
+                        int j = 0;
+                        while(isalnum(exp[i])) temp[j++] = exp[i++];
+                        temp[j] = '\0';
+                        strcpy(operands[++topOpnd],temp);
+                }
+                else {
+                        while(topOp >= 0 && precedence(operators[topOp]) >= precedence(exp[i])) {
+                                char op = operators[topOp--];
+                                if(op == '=') {
+                                        printf("%s = %s\n",operands[topOpnd-1],operands[topOpnd]);
+                                        topOpnd -= 2;
+                                }
+                                else {
+                                        printf("t%d = %s %c %s\n",t,operands[topOpnd-1],op,operands[topOpnd]);
+                                        sprintf(operands[--topOpnd],"t%d",t++);
+                                }
+                        }
+                        operators[++topOp] = exp[i++];
+                }
         }
-    }
-    while (opstktop>=0) //pop remaining operators
-    {
-        char op[2]={'\0'};
-        op[0] = opstack[opstktop--];
-        if(strcmp(op,"="))
-        {
-            printf("t%d = %s %s %s\n",temp_count,opndstack[opndstktop-1],op,opndstack[opndstktop]);
-            sprintf(opndstack[--opndstktop],"t%d",temp_count++);
+        while(topOp >= 0) {
+                char op = operators[topOp--];
+                if(op == '=')
+                        printf("%s = %s\n",operands[topOpnd-1],operands[topOpnd]);
+                else {
+                        printf("t%d = %s %c %s\n",t,operands[topOpnd-1],op,operands[topOpnd]);
+                        sprintf(operands[--topOpnd],"t%d",t++);
+                }
         }
-        else
-            printf("%s = t%d\n",opndstack[opndstktop-1],temp_count-1);       
-    }
 }
-int main()
-{
-    printf("Enter an expression without spaces\n");
-    scanf("%[^\n]",expn);
-    icg_from_infix();
+
+int main() {
+        char exp[MAX];
+        printf("Enter infix expression without space: ");
+        scanf("%[^\n]",exp);
+        generateICG(exp);
+        return 0;
 }
